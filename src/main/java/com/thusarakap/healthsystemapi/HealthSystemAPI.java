74afdb28e5e14fd5d9e5429d2fe09c677d97e54c
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-04-25 00:49:24 +0530

    INITIAL

---
+++ src/main/java/com/thusarakap/healthsystemapi/HealthSystemAPI.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.thusarakap.healthsystemapi;
+
+/**
+ *
+ * @author Thusaraka
+ */
+public class HealthSystemAPI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+ 
