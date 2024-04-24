f6ede5a47646e4792ea68ef19430fa7104ee043f
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-04-25 03:27:27 +0530

    Update Prescription.java

--- src/main/java/com/thusarakap/healthsystemapi/model/Prescription.java
+++ src/main/java/com/thusarakap/healthsystemapi/model/Prescription.java
@@ -9,5 +9,78 @@ package com.thusarakap.healthsystemapi.model;
  * @author Thusaraka
  */
 public class Prescription {
-    
+    private int id;
+    private String medication;
+    private String dosage;
+    private String instructions;
+    private String duration;
+    private Patient patient;
+    private Doctor doctor;
+
+    public Prescription(int id, String medication, String dosage, String instructions, String duration, Patient patient, Doctor doctor) {
+        this.id = id;
+        this.medication = medication;
+        this.dosage = dosage;
+        this.instructions = instructions;
+        this.duration = duration;
+        this.patient = patient;
+        this.doctor = doctor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMedication() {
+        return medication;
+    }
+
+    public void setMedication(String medication) {
+        this.medication = medication;
+    }
+
+    public String getDosage() {
+        return dosage;
+    }
+
+    public void setDosage(String dosage) {
+        this.dosage = dosage;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
 }
+
