751ca1658570dd589d104dd63518b5b6b3cc8f66
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-04-25 01:48:50 +0530

    Update Doctor.java

--- src/main/java/com/thusarakap/healthsystemapi/model/Doctor.java
+++ src/main/java/com/thusarakap/healthsystemapi/model/Doctor.java
@@ -8,6 +8,30 @@ package com.thusarakap.healthsystemapi.model;
  *
  * @author Thusaraka
  */
-public class Doctor {
+public class Doctor extends Person{
+    private String specialisation;
+    private String contactDetails;
     
+    public Doctor(String name, String contactInformation, String address, String specialisation, String contactDetails) {
+        super(name, contactInformation, address);
+        this.specialisation = specialisation;
+        this.contactDetails = contactDetails;
+    }
+    
+    public String getSpecialisation() {
+        return specialisation;
+    }
+    
+    public void setSpecialisation(String specialisation) {
+        this.specialisation = specialisation;
+    }
+    
+    public String getContactDetails() {
+        return contactDetails;
+    }
+    
+    public void setContactDetails(String contactDetails) {
+        this.contactDetails = contactDetails;
+    }
 }
+
