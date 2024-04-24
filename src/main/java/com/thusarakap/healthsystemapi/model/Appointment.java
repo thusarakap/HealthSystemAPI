e4cdbff48656d2725bd1233335de0e336b9ee99f
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-04-25 03:11:04 +0530

    Update Appointment.java

--- src/main/java/com/thusarakap/healthsystemapi/model/Appointment.java
+++ src/main/java/com/thusarakap/healthsystemapi/model/Appointment.java
@@ -4,10 +4,66 @@
  */
 package com.thusarakap.healthsystemapi.model;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 /**
  *
  * @author Thusaraka
  */
 public class Appointment {
+    private int id;
+    private LocalDate date;
+    private LocalTime time;
+    private Patient patient;
+    private Doctor doctor;
     
+    public Appointment(int id, LocalDate date, LocalTime time, Patient patient, Doctor doctor) {
+        this.id = id;
+        this.date = date; 
+        this.time = time;
+        this.patient = patient;
+        this.doctor = doctor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }    
 }
+
