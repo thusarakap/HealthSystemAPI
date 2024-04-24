0f96722e35b78c32a16d4999e114c31a819c6282
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-04-25 03:24:27 +0530

    Update MedicalRecord.java

--- src/main/java/com/thusarakap/healthsystemapi/model/MedicalRecord.java
+++ src/main/java/com/thusarakap/healthsystemapi/model/MedicalRecord.java
@@ -4,10 +4,55 @@
  */
 package com.thusarakap.healthsystemapi.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Thusaraka
  */
 public class MedicalRecord {
+    private Patient patient;
+    private List<String> diagnoses;
+    private List<String> treatments;
+    private List<String> additionalNotes;
+
+    public MedicalRecord(Patient patient) {
+        this.patient = patient;
+        this.diagnoses = new ArrayList<>();
+        this.treatments = new ArrayList<>();
+        this.additionalNotes = new ArrayList<>();
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
     
+    public void addDiagnosis(String diagnosis) {
+        diagnoses.add(diagnosis);
+    }
+    
+    public List<String> getDiagnoses() {
+        return diagnoses;
+    }
+    
+     public void addTreatment(String treatment) {
+        treatments.add(treatment);
+    }
+    
+    public List<String> getTreatments() {
+        return treatments;
+    }
+  
+    public void addAdditionalNotes(String note) {
+        additionalNotes.add(note);
+    }
+    
+    public List<String> getAdditonalNotes() {
+        return additionalNotes;
+    }
 }
