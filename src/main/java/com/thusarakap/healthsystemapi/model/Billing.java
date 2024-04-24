a4268390000de5fafe49b840b85799515f4cd8ce
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-04-25 03:28:00 +0530

    Update Billing.java

--- src/main/java/com/thusarakap/healthsystemapi/model/Billing.java
+++ src/main/java/com/thusarakap/healthsystemapi/model/Billing.java
@@ -9,5 +9,57 @@ package com.thusarakap.healthsystemapi.model;
  * @author Thusaraka
  */
 public class Billing {
-    
+    private int invoiceNumber;
+    private double amount;
+    private boolean paymentStatus;
+    private Patient patient;
+    private Doctor doctor;
+
+    public Billing(int invoiceNumber, double amount, boolean paymentStatus, Patient patient, Doctor doctor) {
+        this.invoiceNumber = invoiceNumber;
+        this.amount = amount;
+        this.paymentStatus = paymentStatus;
+        this.patient = patient;
+        this.doctor = doctor;
+    }
+
+    public int getInvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    public void setInvoiceNumber(int invoiceNumber) {
+        this.invoiceNumber = invoiceNumber;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public boolean isPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(boolean paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
 }
