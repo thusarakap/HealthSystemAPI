a5f46937a14f9dc40a45c496d504eac261ff9c65
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-04-25 02:01:16 +0530

    Update Patient.java

--- src/main/java/com/thusarakap/healthsystemapi/model/Patient.java
+++ src/main/java/com/thusarakap/healthsystemapi/model/Patient.java
@@ -8,6 +8,30 @@ package com.thusarakap.healthsystemapi.model;
  *
  * @author Thusaraka
  */
-public class Patient {
+public class Patient extends Person{
+    private String medicalHistory;
+    private String currentHealthStatus;
+    
+    public Patient(String name, String contactInformation, String address, String medicalHistory, String currentHealthStatus) {
+        super(name, contactInformation, address);
+        this.medicalHistory = medicalHistory;
+        this.currentHealthStatus = currentHealthStatus;
+    }
+    
+    public String getMedicalHistory() {
+        return medicalHistory;
+    }
+    
+    public void setMedicalHistory(String medicalHistory) {
+        this.medicalHistory = medicalHistory;
+    }
+    
+    public String getCurrentHealthStatus() {
+        return currentHealthStatus;
+    }
+    
+    public void setCurrentHealthStatus(String currentHealthStatus) {
+        this.currentHealthStatus = currentHealthStatus;
+    }
     
 }
