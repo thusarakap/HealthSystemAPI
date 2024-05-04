23c09f996044b1bf7d48484e98d7bf9863250f0d
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-05-05 01:33:37 +0530

    Create PersonNotFoundException.java

---
+++ src/main/java/com/thusarakap/healthsystemapi/exception/PersonNotFoundException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.thusarakap.healthsystemapi.exception;
+
+/**
+ *
+ * @author Thusaraka
+ */
+
+public class PersonNotFoundException extends RuntimeException {
+    public PersonNotFoundException(String message) {
+        super(message);
+    }
+}
