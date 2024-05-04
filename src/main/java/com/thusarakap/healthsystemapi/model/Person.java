18c7d4dbefea8ee314e3416c6b0ab8d4ce9c542a
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-05-05 01:32:59 +0530

    Update Person.java

--- src/main/java/com/thusarakap/healthsystemapi/model/Person.java
+++ src/main/java/com/thusarakap/healthsystemapi/model/Person.java
@@ -9,16 +9,18 @@ package com.thusarakap.healthsystemapi.model;
  * @author Thusaraka
  */
 
+/* Model class representing a person. */
 public class Person {
     private int id;
     private String name;
     private String contactInformation;
     private String address;
     
+    /* Default constructor */
     public Person() {
-        // Default constructor needed for Jackson deserialization
     }
     
+    /* Constructor with parameters */
     public Person(int id, String name, String contactInformation, String address) {
         this.id = id;
         this.name = name;
@@ -26,36 +28,43 @@ public class Person {
         this.address = address;
     }
     
+    /* Getter for ID */
     public int getId() {
         return id;
     }
     
+    /* Setter for ID */
     public void setId(int id) {
         this.id = id;
     }
 
+    /* Getter for name */
     public String getName() {
         return name;
     }
     
+    /* Setter for name */
     public void setName(String name) {
         this.name = name;
     }
     
+    /* Getter for contact information */
     public String getContactInformation() {
         return contactInformation;
     }
     
+    /* Setter for contact information */
     public void setContactInformation(String contactInformation) {
         this.contactInformation = contactInformation;
     }
     
+    /* Getter for address */
     public String getAddress() {
         return address;
     }
     
+    /* Setter for address */
     public void setAddress(String address) {
         this.address = address;
     }
 }
-
