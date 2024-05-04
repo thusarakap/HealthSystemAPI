31e292d552dce5940d044e6a4c136f562058d726
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-05-05 01:33:14 +0530

    Update Patient.java

--- src/main/java/com/thusarakap/healthsystemapi/model/Patient.java
+++ src/main/java/com/thusarakap/healthsystemapi/model/Patient.java
@@ -9,32 +9,38 @@ package com.thusarakap.healthsystemapi.model;
  * @author Thusaraka
  */
 
-public class Patient extends Person{
+/* Model class representing a patient, extending the Person class. */
+public class Patient extends Person {
     private String medicalHistory;
     private String currentHealthStatus;
     
+    /* Default constructor */
     public Patient() {
-        // Default constructor needed for Jackson deserialization
     }
     
+    /* Constructor with parameters */
     public Patient(int id, String name, String contactInformation, String address, String medicalHistory, String currentHealthStatus) {
         super(id, name, contactInformation, address);
         this.medicalHistory = medicalHistory;
         this.currentHealthStatus = currentHealthStatus;
     }
     
+    /* Getter for medical history */
     public String getMedicalHistory() {
         return medicalHistory;
     }
     
+    /* Setter for medical history */
     public void setMedicalHistory(String medicalHistory) {
         this.medicalHistory = medicalHistory;
     }
     
+    /* Getter for current health status */
     public String getCurrentHealthStatus() {
         return currentHealthStatus;
     }
     
+    /* Setter for current health status */
     public void setCurrentHealthStatus(String currentHealthStatus) {
         this.currentHealthStatus = currentHealthStatus;
     }
